0d039e8c1eb9d383b4eb1e3688e1788eeb265e72
muhammadsolihin <salihin@vhost.id>
2015-10-05 19:07:46 +0700

    Front End

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Biodata Indobit</title>
+        <!-- Bootstrap CSS -->
+        <link href="assets/css/vendor/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/flat-ui-pro.min.css" rel="stylesheet">
+        <style type="text/css">
+        .border{
+            border-radius: 0px !important;
+        }
+        .gambar{
+        padding: 0px 110px !important;
+        margin-bottom: 20px !important;
+        margin-top: 20px;
+        }
+        .user-pass{
+        margin-bottom: 20px !important;
+        margin-top: 20px;
+        }
+        .img{
+        margin-bottom: -25px !important;
+        margin-top: -10px;
+        }
+        .partner{
+        margin-bottom: 60px !important;
+        margin-top: 40px;
+        }
+        .bener{
+        margin-bottom: 0px !important;
+        background-color: #002635 !important;
+        }
+        .background{
+        background-color: /*#149279*/#002635 !important;
+        padding: 10px 100px;
+        /*background-image: images/bg-blue.png !important;*/
+        }
+        .background1{
+        background: #000000/*#2e3539*/ !important;
+        padding: 10px 100px;
+        }
+        .background2{
+        background: #34495E !important;
+        color: white;
+        /*padding: 10px 100px;*/
+        }
+        .font{
+        font-size: 50px;
+        color: white;
+        }
+        .font2{
+        font-size: 15px;
+        color: white;
+        }
+        .font1{
+        font-size: 15px;
+        color: white;
+        }
+        </style>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:localhost// -->
+        <!--[if lt IE 9]>
+        <script src="https:localhost//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https:localhost//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default bener border" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+                <img class="gambar" height="50" src="images/logo-header.png" alt="Image">
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control user-pass" placeholder="username">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control user-pass" placeholder="password">
+                    </div>
+                    <button class="btn btn-danger">Submit</button>
+                </form>
+            </div>
+        </nav>
+        <img src="images/bener.jpg" class="img-responsive" alt="Image">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <legend></legend>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <img src="images/ds.png" class="img-responsive" alt="Image">
+            </div>
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                <a href="#">Muhammad Solihin</a><br>
+                Jabatan<br>
+                Email<br>
+            </div>
+            <center>
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                Sosial Media : <br><a href="#fakelink" class="fui-facebook"></a> &nbsp &nbsp<a href="#fakelink" class="fui-twitter"></a>&nbsp &nbsp<a href="#fakelink" class="fui-instagram"></a>  &nbsp <a href="#fakelink" class="fui-skype"></a>
+            </div>
+            </center>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <legend></legend><hr>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <img src="images/ds.png" class="img-responsive" alt="Image">
+            </div>
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                <a href="#">Muhammad Solihin</a><br>
+                Jabatan<br>
+                Email<br>
+            </div>
+            <center>
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                Sosial Media : <br><a href="#fakelink" class="fui-facebook"></a>&nbsp &nbsp<a href="#fakelink" class="fui-twitter"></a>&nbsp &nbsp<a href="#fakelink" class="fui-instagram"></a>  &nbsp <a href="#fakelink" class="fui-skype"></a>
+                <legend></legend><br>
+            </div>
+            </center>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 background">
+            <a class="font"><p align="right">
+                I n d o b i t &nbsp  T e c h n o l o g y <img src="images/bg-blue.png" class="img" alt="Image">
+            </a></p>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 partner">
+            <legend></legend>
+            <center>
+            <img src="images/ds.png" class="" alt="Image"> &nbsp &nbsp &nbsp<img src="images/ds.png" class="" alt="Image">
+            <!-- <legend></legend> -->
+        </div>
+        </center>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 background font2">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 partner">
+            INDOBIT TECHNOLOGY <br>
+            Kami adalah sekumpulan anak muda gagah berani tak takut mati walau badai menghampiri
+        </div>
+        <!-- <legend></legend><br> -->
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 partner">
+            Hubungi Kami <br>
+            Jl.M.H.Tamrin <br>
+            jakarta
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 partner">
+            Partner <br>
+            Jl.M.H.Tamrin <br>
+            jakarta
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 background1 font1">
+        <center>Copyright©2015 Indobit Technologies</center>
+    </div>
+    <!-- jQuery -->
+    <script src="assets/js/flat-ui-pro.min.js"></script>
+    <script src="assets/js/vendor/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+</body>
+</html>
